28fff688035e6fbde82e2cf1b359535997b18565
ClaxBitt <claxbitt8@gmail.com>
2024-02-06 13:23:45 -0600

    weather http to https

--- countriesdata/src/services/weather.js
+++ countriesdata/src/services/weather.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const WEATHER_API_KEY = import.meta.env.VITE_API_KEY
-const URL = `http://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}`
+const URL = `https://api.weatherapi.com/v1/current.json?key=${WEATHER_API_KEY}`
 
 const getCapitalWeather = (capital) => {
   const request = axios.get(`${URL}&q=${capital}`)
